e769a002581e633acfd51c7e05123661807ef079
ZynLuc <zynluc@protonmail.com>
2021-01-20 01:42:34 +0000

    add

--- js/ads.js
+++ js/ads.js
@@ -1,20 +1,49 @@
-class Ads extends React.Component {
+class Table extends React.Component {
+  constructor(props) {
+     super(props) 
+     this.state = { 
+        componentes: [
+           { id: 1, disciplina: 'Sistemas Operacionais', carga: 66.7, periodo: 'Noturno' },
+           { id: 2, disciplina: 'Inglês', carga: 33.3, periodo: 'Noturno' },
+           { id: 3, disciplina: 'Laboratório de Estruturas de Dados e Programação', carga: 33.3, periodo: 'Noturno' },
+           { id: 4, disciplina: 'Introdução a Banco de Dados', carga: 66.7, periodo: 'Noturno' }
+        ]
+     }
+  }
 
-    constructor() {
-      super();
-    }
-  
-    render() {   
-      return <div>
-        <p>O <b>Instituto Federal de Educação, Ciência e Tecnologia de São Paulo</b> – é uma autarquia federal de ensino.</p>
-        <p>Fundada em 1909, como Escola de Aprendizes Artífices, é reconhecida pela sociedade paulista por sua excelência no ensino público gratuito de qualidade.</p>
-        <p>Durante sua história, recebeu, também, os nomes de Escola Técnica Federal de São Paulo e Centro Federal de Educação Tecnológica de São Paulo. Com a transformação em Instituto, em dezembro de 2008, passou a ter relevância de universidade, destacando-se pela autonomia.</p>
-        <p>Com a mudança, o Instituto Federal de São Paulo passou a destinar 50% das vagas para os cursos técnicos e, no mínimo, 20% das vagas para os cursos de licenciatura, sobretudo nas áreas de Ciências e da Matemática. Complementarmente, continuará oferecendo cursos de formação inicial e continuada, tecnologias, engenharias e pós-graduação.</p>
-        <p>Além dos cursos presenciais, o Instituto Federal de São Paulo oferece os cursos Técnicos em Administração e em Informática para Internet e, a partir de 2012, o superior de Formação de Professores na modalidade de Ensino a Distância (EaD).</p>
-        <p>O IFSP é organizado em diversos câmpus e possui mais de 40 mil alunos matriculados nas 36 unidades distribuídas pelo estado de São Paulo.;</p>
-        <p>Localizado em <b>Rua Pedro Vicente, 625 - Canindé - São Paulo - SP - Brasil - Cep: 01109-010</b></p>
-      </div>;
-    }
-}  
+   renderTableHeader() {
+     let header = Object.keys(this.state.componentes[0]).splice(1,3);      
+     console.log(header);
+     return header.map((key, index) => {
+        return <th key={index}>{key.toUpperCase()}</th>
+     })
+  }
 
-ReactDOM.render(<Sobre />, document.getElementsByClassName('table-component'));
+  renderTableData() {
+     return this.state.componentes.map((componente, index) => {
+        const { id, disciplina, carga, periodo } = componente //destructuring
+        return (
+           <tr key={id}>               
+              <td>{disciplina}</td>
+              <td>{carga}</td>
+              <td>{periodo}</td>
+           </tr>
+        )
+     })
+  }
+  render() { 
+     return (
+        <div>
+           <h2 id='title'>Componentes curriculares</h2>
+           <table id='componentes'>
+              <tbody>
+                 <tr>{this.renderTableHeader()}</tr>
+                 {this.renderTableData()}
+              </tbody>
+           </table>
+        </div>
+     )
+  }
+}
+
+ReactDOM.render(<Table />, document.getElementsByClassName('table-component'));
